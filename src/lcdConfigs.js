73a97f3455d9a243a5f2065f2977b3a01710401b
Thomas Wostyn <thomas.wostyn@gmail.com>
2024-04-28 15:39:23 +1000

    Use https://terra-rest.publicnode.com

--- src/lcdConfigs.js
+++ src/lcdConfigs.js
@@ -1,5 +1,5 @@
 const lcds = [
-  ['terra', 'https://rest.cosmos.directory/terra2'],
+  ['terra', 'https://terra-rest.publicnode.com'],
   ['kujira', 'https://kujira-api.polkachu.com'],
   ['neutron', 'https://neutron-rest.publicnode.com'],
   ['migaloo', 'https://migaloo-api.polkachu.com'],
