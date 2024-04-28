36031509ae2dccee483250c60f0c04d8ed1731d9
Thomas WOSTYN <thomas.wostyn@gmail.com>
2024-04-28 22:12:32 +1000

    Missing file

---
+++ src/lsds/chihuahua.js
@@ -0,0 +1,23 @@
+const ampHuahua = {
+  name: 'HUAHUA → ampHUAHUA',
+  dex: 'White Whale Chihuahua',
+  stakingContract: {
+    contract: 'chihuahua1nktfhalzvtx82kyn4dh6l8htcl0prfpnu380a39zj52nzu3j467qqg23ry',
+    exchangeRate: (data) => data.exchange_rate,
+  },
+  offerNativeTokenDenom: 'uhuahua',
+  poolContract: 'chihuahua1a6xwgvyvrmzgue6hectem3fwdzquny44a4y20a9wvlrtalhlsk9sryz5t9',
+};
+
+const bHuahua = {
+  name: 'HUAHUA → bHUAHUA',
+  dex: 'White Whale Chihuahua',
+  stakingContract: {
+    contract: 'chihuahua1psf89r2g9vdlttrjphspcpzzfx87r2r4nl5fg703ky42mp2706wsw5330f',
+    exchangeRate: (data) => data.exchange_rate,
+  },
+  offerNativeTokenDenom: 'uhuahua',
+  poolContract: 'chihuahua1py86y6946ed07g8v24thess2havjjgpg3uvjdu4v805czmge37hsvlt6qz',
+};
+
+export const chihuahuaLsds = [ampHuahua, bHuahua];
