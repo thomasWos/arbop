53ae9d19da3a53a540cb401ebbe5e06c765ec503
Thomas Wostyn <thomas.wostyn@gmail.com>
2024-04-29 22:18:38 +1000

    Add boneWHALEt

--- src/lsds/migaloo.js
+++ src/lsds/migaloo.js
@@ -31,4 +31,15 @@ const ampWhaleT = {
   poolContract: 'migaloo1ugv3g8lckm70np3u50u5wetnv2dfda073gyazy2v50t5c3wza3xqj5drtk',
 };
 
-export const whaleLsds = [ampWhaleMigaloo, ampWhaleTerra, ampWhaleT];
+const bonewhaleT = {
+  name: 'bWHALE → bWHALEt',
+  dex: 'White Whale Migaloo',
+  stakingContract: {
+    contract: 'terra10j3zrymfrkta2pxe0gklc79gu06tqyuy8c3kh6tqdsrrprsjqkrqzfl4df',
+    exchangeRate: (data) => data.exchange_rate,
+  },
+  offerNativeTokenDenom: 'factory/migaloo1mf6ptkssddfmxvhdx0ech0k03ktp6kf9yk59renau2gvht3nq2gqdhts4u/boneWhale',
+  poolContract: 'migaloo1azqqmeg7zcj9vdtqpc65dmr2fkmkf3x6dcyhnau6d6er0w2r3arq470dzj',
+};
+
+export const whaleLsds = [ampWhaleMigaloo, ampWhaleTerra, ampWhaleT, bonewhaleT];
