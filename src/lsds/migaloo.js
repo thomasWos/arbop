c65f064f8c6018bb0ccac28e6b25afb78be5ca38
Thomas Wostyn <thomas.wostyn@gmail.com>
2024-05-06 09:19:48 +1000

    Add bonewhale

--- src/lsds/migaloo.js
+++ src/lsds/migaloo.js
@@ -5,6 +5,7 @@ const ampWhaleMigaloo = {
     contract: 'migaloo1436kxs0w2es6xlqpp9rd35e3d0cjnw4sv8j3a7483sgks29jqwgshqdky4',
     exchangeRate: (data) => data.exchange_rate,
   },
+  unboundingPeriod: 21 + 3,
   offerNativeTokenDenom: 'uwhale',
   poolContract: 'migaloo1ull9s4el2pmkdevdgrjt6pwa4e5xhkda40w84kghftnlxg4h3knqpm5u3n',
 };
@@ -16,6 +17,7 @@ const ampWhaleTerra = {
     contract: 'migaloo1436kxs0w2es6xlqpp9rd35e3d0cjnw4sv8j3a7483sgks29jqwgshqdky4',
     exchangeRate: (data) => data.exchange_rate,
   },
+  unboundingPeriod: 21 + 3,
   offerNativeTokenDenom: 'ibc/36A02FFC4E74DF4F64305130C3DFA1B06BEAC775648927AA44467C76A77AB8DB',
   poolContract: 'terra1ntx595elf3ukxcd0wy76h24rzztcv2p6n3wmfd358ks95prv42fs9mn63n',
 };
@@ -31,6 +33,30 @@ const ampWhaleT = {
   poolContract: 'migaloo1ugv3g8lckm70np3u50u5wetnv2dfda073gyazy2v50t5c3wza3xqj5drtk',
 };
 
+const bonewhale = {
+  name: 'WHALE → bWHALE',
+  dex: 'White Whale Migaloo',
+  stakingContract: {
+    contract: 'migaloo1mf6ptkssddfmxvhdx0ech0k03ktp6kf9yk59renau2gvht3nq2gqdhts4u',
+    exchangeRate: (data) => data.exchange_rate,
+  },
+  unboundingPeriod: 21 + 3,
+  offerNativeTokenDenom: 'uwhale',
+  poolContract: 'migaloo1dg5jrt89nddtymjx5pzrvdvdt0m4zl3l2l3ytunl6a0kqd7k8hss594wy6',
+};
+
+const bonewhaleTerra = {
+  name: 'WHALE → bWHALE',
+  dex: 'White Whale Terra',
+  stakingContract: {
+    contract: 'migaloo1436kxs0w2es6xlqpp9rd35e3d0cjnw4sv8j3a7483sgks29jqwgshqdky4',
+    exchangeRate: (data) => data.exchange_rate,
+  },
+  unboundingPeriod: 21 + 3,
+  offerNativeTokenDenom: 'ibc/36A02FFC4E74DF4F64305130C3DFA1B06BEAC775648927AA44467C76A77AB8DB',
+  poolContract: 'terra1j9jmsplecj9ay2py27953p84nfmv7f6ce75ms5fleyhd0aecpc7q0hgmsa',
+};
+
 const bonewhaleT = {
   name: 'bWHALE → bWHALEt',
   dex: 'White Whale Migaloo',
@@ -42,4 +68,4 @@ const bonewhaleT = {
   poolContract: 'migaloo1azqqmeg7zcj9vdtqpc65dmr2fkmkf3x6dcyhnau6d6er0w2r3arq470dzj',
 };
 
-export const whaleLsds = [ampWhaleMigaloo, ampWhaleTerra, ampWhaleT, bonewhaleT];
+export const whaleLsds = [ampWhaleMigaloo, ampWhaleTerra, ampWhaleT, bonewhale, bonewhaleTerra, bonewhaleT];
