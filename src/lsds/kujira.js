27e4e2a0fa74e1fef7dbfd9f82fb2a5913318fd7
Thomas Wostyn <thomas.wostyn@gmail.com>
2024-05-19 18:29:29 +1000

    add stOsmo Kujira

--- src/lsds/kujira.js
+++ src/lsds/kujira.js
@@ -22,4 +22,13 @@ const qcKUJIFin = {
   poolContract: 'kujira14wsrem89304kpkl6d0j58dl6479eekwt047ccs0qzv9f60w80wzs8rjq4j',
 };
 
-export const kujiLsds = [ampKujiFin, qcKUJIFin];
+const stOsmoKujira = {
+  name: 'OSMO → stOsmo',
+  dex: 'FIN',
+  redemptionKey: 'strideOsmo',
+  unboundingPeriod: 17,
+  offerNativeTokenDenom: 'ibc/47BD209179859CDE4A2806763D7189B6E6FE13A17880FE2B42DE1E6C1E329E23',
+  poolContract: 'kujira14rrh06m6rm699psw4dly88n7whqjsustqzcj23rwvmy5egu4vd8qm99rcx',
+};
+
+export const kujiLsds = [ampKujiFin, qcKUJIFin, stOsmoKujira];
