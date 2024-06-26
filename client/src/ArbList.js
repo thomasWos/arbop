4911d80fdce2b51f626774abb8357937080841b6
Thomas Wostyn <thomas.wostyn@gmail.com>
2024-06-26 21:50:07 +1000

    Display more robust

--- client/src/ArbList.js
+++ client/src/ArbList.js
@@ -6,9 +6,9 @@ const ArbList = ({ arbs }) => {
       {arbs.map((arb) => (
         <div key={arb.id} className={`arb-item ${arb.arb < 0.2 ? 'low-arb' : ''}`}>
           <div className="arb-name">{arb.name}</div>
-          <div className="arb-percentage">{arb.arb.toFixed(3)}%</div>
+          <div className="arb-percentage">{arb.arb && `${arb.arb.toFixed(3)}%`}</div>
           <div className="arb-dex">{arb.dex}</div>
-          <div className="arb-apy">{arb.apy ? `≈ ${arb.apy.toFixed(1)}% APY` : ''}</div>
+          <div className="arb-apy">{arb.apy && `≈ ${arb.apy.toFixed(1)}% APY`}</div>
         </div>
       ))}
     </div>
