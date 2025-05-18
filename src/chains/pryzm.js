febbb59c4b851babf15b2b519b680f8b86b6f5b6
Thomas WOSTYN <thomas.wostyn@gmail.com>
2025-05-18 13:25:31 +1000

    Add dexUrl for prysm

--- src/chains/pryzm.js
+++ src/chains/pryzm.js
@@ -25,6 +25,7 @@ const lunaTocLuna = {
 const cLunaToLuna = {
   name: 'cLUNA → LUNA',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'cLUNAinv',
   tokenIn: 'c:uluna',
   tokenOut: 'ibc/B8AF5D92165F35AB31F3FC7C7B444B9D240760FA5D406C49D24862BD0284E395',
@@ -34,6 +35,7 @@ const cLunaToLuna = {
 const atomTocAtom = {
   name: 'ATOM → cATOM',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'cATOM',
   unboundingPeriod: 21 + 3,
   tokenIn: 'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2',
@@ -44,6 +46,7 @@ const atomTocAtom = {
 const cAtomToAtom = {
   name: 'cATOM → ATOM',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'cATOMinv',
   tokenIn: 'c:uatom',
   tokenOut: 'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2',
@@ -53,6 +56,7 @@ const cAtomToAtom = {
 const atomTodAtom = {
   name: 'ATOM → dATOM',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'dATOM',
   tokenIn: 'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2',
   tokenOut: 'ibc/EA6E1E8BA2EB9F681C4BD12C8C81A46530A62934F2BD561B120A00F46946CE87',
@@ -62,6 +66,7 @@ const atomTodAtom = {
 const dAtomToAtom = {
   name: 'dATOM → ATOM',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'dATOMinv',
   tokenIn: 'ibc/EA6E1E8BA2EB9F681C4BD12C8C81A46530A62934F2BD561B120A00F46946CE87',
   tokenOut: 'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2',
@@ -71,6 +76,7 @@ const dAtomToAtom = {
 const osmoTocOsmo = {
   name: 'OSMO → cOSMO',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'cOSMO',
   unboundingPeriod: 14 + 3,
   tokenIn: 'ibc/13B2C536BB057AC79D5616B8EA1B9540EC1F2170718CAFF6F0083C966FFFED0B',
@@ -81,6 +87,7 @@ const osmoTocOsmo = {
 const cOsmoToOsmo = {
   name: 'cOSMO → OSMO',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'cOSMOinv',
   tokenIn: 'c:uosmo',
   tokenOut: 'ibc/13B2C536BB057AC79D5616B8EA1B9540EC1F2170718CAFF6F0083C966FFFED0B',
@@ -90,6 +97,7 @@ const cOsmoToOsmo = {
 const injTocInj = {
   name: 'INJ → cINJ',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'cINJ',
   unboundingPeriod: 21 + 3,
   decimal: 18,
@@ -101,6 +109,7 @@ const injTocInj = {
 const cInjToInj = {
   name: 'cINJ → INJ',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'cINJinv',
   decimal: 18,
   tokenIn: 'c:inj',
@@ -111,6 +120,7 @@ const cInjToInj = {
 const usdcToNusdc = {
   name: 'USDC → nUSDC',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'nUSDC',
   tokenIn: 'ibc/BFAAB7870A9AAABF64A7366DAAA0B8E5065EAA1FCE762F45677DC24BE796EF65',
   tokenOut: 'ibc/9924F230D93D88A98D1469A24E523D7BF58935E6321C18CFAEFBFC068B54676F',
@@ -120,6 +130,7 @@ const usdcToNusdc = {
 const nUsdcToUsdc = {
   name: 'nUSDC → USDC',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'nUSDCinv',
   tokenIn: 'ibc/9924F230D93D88A98D1469A24E523D7BF58935E6321C18CFAEFBFC068B54676F',
   tokenOut: 'ibc/BFAAB7870A9AAABF64A7366DAAA0B8E5065EAA1FCE762F45677DC24BE796EF65',
@@ -129,6 +140,7 @@ const nUsdcToUsdc = {
 const usdtToNusdt = {
   name: 'USDT → nUSDT',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'nUSDT',
   tokenIn: 'ibc/D8A36AE90F20FE4843A8D249B1BCF0CCDDE35C4B605C8DED57BED20C639162D0',
   tokenOut: 'ibc/018AC5A86B24C7FCE5BAF37B50F47028A8BA791F64233773531EE10F97CBA225',
@@ -138,6 +150,7 @@ const usdtToNusdt = {
 const nUsdtToUsdt = {
   name: 'nUSDT → USDT',
   dex: 'Prysm',
+  dexUrl: 'https://app.pryzm.zone/swap',
   redemptionKey: 'nUSDTinv',
   tokenIn: 'ibc/018AC5A86B24C7FCE5BAF37B50F47028A8BA791F64233773531EE10F97CBA225',
   tokenOut: 'ibc/D8A36AE90F20FE4843A8D249B1BCF0CCDDE35C4B605C8DED57BED20C639162D0',
